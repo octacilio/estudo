73c86ec9b1215300c5280364cefdcb425944740a
octacilio <octanaza@gmail.com>
2023-05-03 10:33:56 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<DOCTYPE html>
+<html>
+<head lang="Pt-br">
+  <title>Primeiro Repositorio</title>
+</head>
+<body>
+  <h3>
+    <?php>
+      echo "Este é o meu primeiro Repositório!"
+    <?>
+  </h3>
+</body>
+</html>
